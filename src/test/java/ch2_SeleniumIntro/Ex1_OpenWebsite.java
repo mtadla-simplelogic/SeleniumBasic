b97b45c7653e1edeaa454deba67277c1b4e03843
Mateusz Tadla <mta@scalepoint.com>
2022-05-14 14:04:04 +0200

    implement first selenium tests

--- src/test/java/ch2_SeleniumIntro/Ex1_OpenWebsite.java
+++ src/test/java/ch2_SeleniumIntro/Ex1_OpenWebsite.java
@@ -4,21 +4,29 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class Ex1_OpenWebsite {
+    WebDriver driver;
 
     @Test
-    public void shouldOpenWebsite(){
+    public void shouldOpenWebsite() {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://seleniumui.moderntester.pl/");
 
+        Assert.assertEquals(1, 2);
+    }
+
+    @AfterMethod
+    public void cleanUp() {
         driver.quit();
     }
 }
