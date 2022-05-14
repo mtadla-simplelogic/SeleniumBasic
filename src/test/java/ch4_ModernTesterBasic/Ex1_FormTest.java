86d50258c0d4d2b8a4071212ec71f32de78f7812
Mateusz Tadla <mta@scalepoint.com>
2022-05-14 16:00:47 +0200

    add form test

--- src/test/java/ch4_ModernTesterBasic/Ex1_FormTest.java
+++ src/test/java/ch4_ModernTesterBasic/Ex1_FormTest.java
@@ -7,25 +7,44 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Random;
 
 public class Ex1_FormTest extends TestBase {
 
     @Test
-    public void shouldFillFormWithSuccess(){
+    public void shouldFillFormWithSuccess() {
         driver.get("https://seleniumui.moderntester.pl/form.php");
 
         WebElement firstName = driver.findElement(By.id("inputFirstName3"));
         firstName.sendKeys("Jan");
 
-
         driver.findElement(By.id("inputLastName3")).sendKeys("Kowalski");
+        driver.findElement(By.id("inputEmail3")).sendKeys("j.k@w.pl");
+        driver.findElement(By.id("inputAge3")).sendKeys("45");
 
         List<WebElement> genders = driver.findElements(By.name("gridRadiosSex"));
         genders.get(0).click();
 
+        getRandomElement(driver.findElements(By.name("gridRadiosExperience")))
+                .click();
+
+        List<WebElement> professions = driver.findElements(By.name("gridCheckboxProfession"));
+        Assert.assertTrue(professions.size() > 0);
+
+        for (WebElement profession: professions){
+            profession.click();
+        }
+
         //tutaj dopisz dalsza czest testu
+
+        driver.findElement(By.className("btn-primary")).click();
 
         String msg = driver.findElement(By.id("validator-message")).getText();
         Assert.assertEquals(msg, "Form send with success!");
+    }
+
+    public WebElement getRandomElement(List<WebElement> elements) {
+        Random rnd = new Random();
+        return elements.get(rnd.nextInt(elements.size()));
     }
 }
