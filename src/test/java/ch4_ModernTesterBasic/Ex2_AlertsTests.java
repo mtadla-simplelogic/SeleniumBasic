146f317f54d0670003881b96722d9ad0bb65a7b5
Mateusz Tadla <mta@scalepoint.com>
2022-05-15 12:39:57 +0200

    add aletrs

--- src/test/java/ch4_ModernTesterBasic/Ex2_AlertsTests.java
+++ src/test/java/ch4_ModernTesterBasic/Ex2_AlertsTests.java
@@ -1,16 +1,24 @@
 package ch4_ModernTesterBasic;
 
 import ch3_TestBase.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Ex2_AlertsTests extends TestBase {
 
     @BeforeMethod
-    public void testSetup(){
+    public void testSetup() {
         driver.get("https://seleniumui.moderntester.pl/alerts.php");
     }
 
+    @Test
+    public void shouldAcceptAlert(){
+        driver.findElement(By.cssSelector("#simple-alert")).click();
+        driver.switchTo().alert().accept();
 
-
-
+        String msg = driver.findElement(By.cssSelector("#simple-alert-label")).getText();
+        Assert.assertEquals(msg, "OK button pressed");
+    }
 }
