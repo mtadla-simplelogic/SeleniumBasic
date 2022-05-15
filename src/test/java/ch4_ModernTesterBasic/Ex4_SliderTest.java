6419866389c4f12b02e5c5ad6bf4e896b1f13b93
Mateusz Tadla <mta@scalepoint.com>
2022-05-15 16:27:01 +0200

    add slider test

--- src/test/java/ch4_ModernTesterBasic/Ex4_SliderTest.java
+++ src/test/java/ch4_ModernTesterBasic/Ex4_SliderTest.java
@@ -4,6 +4,7 @@ import ch3_TestBase.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Ex4_SliderTest extends TestBase {
@@ -12,8 +13,37 @@ public class Ex4_SliderTest extends TestBase {
     public void shouldMoveSlider() {
         driver.get("https://seleniumui.moderntester.pl/slider.php");
 
-        WebElement handle = driver.findElement(By.cssSelector("#custom-handle"));
-        handle.sendKeys(Keys.ARROW_RIGHT);
+        Assert.assertEquals(getActualSliderValue(), 0);
+
+        moveSliderTo(50);
+        moveSliderTo(30);
+        moveSliderTo(30);
+        moveSliderTo(20);
+        moveSliderTo(65);
+
     }
 
+    private void moveSliderTo(int expectedValue) {
+        if (expectedValue > getActualSliderValue()) {
+            moveSlider(expectedValue, Keys.ARROW_RIGHT);
+        } else if (expectedValue < getActualSliderValue()) {
+            moveSlider(expectedValue, Keys.ARROW_LEFT);
+        }
+        Assert.assertEquals(getActualSliderValue(), expectedValue);
+    }
+
+    private void moveSlider(int expectedValue, Keys keys) {
+        while (expectedValue != getActualSliderValue()) {
+            getSliderHandle().sendKeys(keys);
+        }
+    }
+
+    private int getActualSliderValue() {
+        // "50" -> 50
+        return Integer.parseInt(getSliderHandle().getText());
+    }
+
+    private WebElement getSliderHandle() {
+        return driver.findElement(By.cssSelector("#custom-handle"));
+    }
 }
