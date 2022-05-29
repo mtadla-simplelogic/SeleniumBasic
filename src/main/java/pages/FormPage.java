134bdeaf60e8885780c69a00144878f3d7d35640
Mateusz Tadla <mtadla@sii.pl>
2022-05-29 10:21:14 +0200

    basket waits

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -1,10 +1,23 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class FormPage {
     public FormPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "#inputFirstName3")
+    private WebElement firstName;
+
+    // 3 pola: lastName, email, age
+    // 3 metody: setLastName, setEmail, setAge
+    // dopisac uzycie tych 3 metod w teście
+
+    public void setFirstName(String firstName){
+        this.firstName.sendKeys(firstName);
     }
 }
--- src/test/java/ch7_Pop/Ex1_FormPop.java
+++ src/test/java/ch7_Pop/Ex1_FormPop.java
@@ -5,8 +5,11 @@ import org.testng.annotations.Test;
 import pages.FormPage;
 
 public class Ex1_FormPop extends TestBase {
+
     @Test
-    public void shouldFillFormWithSuccess(){
+    public void shouldFillFormWithSuccess() {
+        driver.get("https://seleniumui.moderntester.pl/form.php");
         FormPage formPage = new FormPage(driver);
+        formPage.setFirstName("Jan");
     }
 }
