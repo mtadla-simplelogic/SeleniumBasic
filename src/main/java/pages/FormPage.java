13aa366b1ac238e9fd4bdcfd4ef3ae5185a502ec
Mateusz Tadla <mtadla@sii.pl>
2022-05-29 10:54:01 +0200

    basket waits

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.Random;
 
 public class FormPage {
     public FormPage(WebDriver driver) {
@@ -22,6 +26,24 @@ public class FormPage {
     @FindBy(css = "#inputAge3")
     private WebElement age;
 
+    @FindBy(css = "#validator-message")
+    private WebElement validationMsg;
+
+    @FindBy(css = "#selectContinents")
+    private WebElement selectContinents;
+
+    @FindBy(css = "[name='gridRadiosSex']")
+    private List<WebElement> genders;
+
+
+    public void selectContinent(String continent){
+        new Select(selectContinents).selectByValue(continent);
+    }
+
+    public void selectRandomGender(){
+        getRandomElement(genders).click();
+    }
+
     public void setFirstName(String firstName) {
         this.firstName.sendKeys(firstName);
     }
@@ -37,4 +59,13 @@ public class FormPage {
         this.age.sendKeys(age);
     }
 
+    public String getValidationMsg(){
+        return validationMsg.getText();
+    }
+
+    private WebElement getRandomElement(List<WebElement> elements) {
+        Random rnd = new Random();
+        int randomListIndex = rnd.nextInt(elements.size());
+        return elements.get(randomListIndex);
+    }
 }
--- src/test/java/ch7_Pop/Ex1_FormPop.java
+++ src/test/java/ch7_Pop/Ex1_FormPop.java
@@ -1,6 +1,7 @@
 package ch7_Pop;
 
 import ch3_TestBase.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormPage;
 
@@ -9,10 +10,17 @@ public class Ex1_FormPop extends TestBase {
     @Test
     public void shouldFillFormWithSuccess() {
         driver.get("https://seleniumui.moderntester.pl/form.php");
+
         FormPage formPage = new FormPage(driver);
+
         formPage.setFirstName("Jan");
         formPage.setLastName("Kowalski");
         formPage.setEmail("j.k23@wp.pl");
         formPage.setAge("23");
+        formPage.selectRandomGender();
+        formPage.selectContinent("europe");
+
+
+        Assert.assertEquals(formPage.getValidationMsg(), "Form send with success");
     }
 }
